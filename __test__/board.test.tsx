5639ffbf16f45b813781a53cc4c02d881956e9dc
Zoeyyhc <najumcao@gmail.com>
2025-01-04 19:06:43 +1100

    Create test for board.

---
+++ __test__/board.test.tsx
@@ -0,0 +1,13 @@
+import Board from "@/components/board";
+import { render} from "@testing-library/react";
+
+describe('Board', () => {
+    it('should render the board with 16 cells', () => {
+        const {container} = render(<Board />);
+        const cellElements = container.querySelectorAll('.cell');
+
+        expect(cellElements.length).toBe(16);
+    });
+});
+
+
