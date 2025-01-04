2aa03500e9792061cb4576b3d1438b8afa65571b
Zoeyyhc <najumcao@gmail.com>
2025-01-04 17:26:36 +1100

    Set up game board.

---
+++ components/board.tsx
@@ -0,0 +1,20 @@
+import styles from "@/styles/board.module.css";
+import { JSX } from "react";
+
+export default function Board() {
+  const renderGrid = () => {
+    const cells: JSX.Element[] = [];
+    const totalCellsCount = 16;
+    for (let i = 0; i < totalCellsCount; i++) {
+      cells.push(<div key={i} className={styles.cell}></div>);
+    }
+
+    return cells;
+  };
+
+  return (
+    <div className={styles.board}>
+      <div className={styles.grid}>{renderGrid()}</div>
+    </div>
+  );
+}
